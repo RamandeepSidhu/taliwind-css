f215493990e12a753cea5fbabe042190446e00d7
RamandeepSidhu <117151940+RamandeepSidhu@users.noreply.github.com>
2024-03-06 10:41:09 +0530

    icon changed

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  contactForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.contactForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', [Validators.required]]
+    });
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      console.log('Form submitted:', this.contactForm.value);
+    } else {
+      this.contactForm.markAllAsTouched();
+    }
+  }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,41 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+      confirm_password:['', [Validators.required]],
+      message: ['', [Validators.required]]
+    });
+  }
+
+  get password() {
+    return this.registerForm.get('password');
+  }
+
+  get email() {
+    return this.registerForm.get('email');
+  }
+  get confirm_password(){
+    return this.registerForm.get('confirm_password');
+
+  }
+
+  get message() {
+    return this.registerForm.get('message');
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Form submitted:', this.registerForm.value);
+    } else {
+      console.log(this.registerForm)
+
+      this.registerForm.markAllAsTouched();
+    }
+    
+  }
 }
